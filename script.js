14f9ae3ff95fd0661763edf27414313533db5201
Mohammad Adeel <Adeel.A.Mhd@gmail.com>
2025-05-18 22:51:03 -0400

    Update index.html, script, and styles

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     e.preventDefault();
     const target = document.querySelector(this.getAttribute('href'));
     if (target) {
-      target.scrollIntoView({
-        behavior: 'smooth'
-      });
+      target.scrollIntoView({ behavior: 'smooth' });
     }
   });
 });
